5bf5feded8d674f66f84b732f87ba1d96c29e6e3
Marco C. Siqueira <63791138+marcosiqueira@users.noreply.github.com>
2025-04-01 23:02:03 -0300

    Folder and File Removal
    The process deletes files and folders, overwriting the data to make recovery irreversible.

---
+++ remove.ps1
@@ -0,0 +1,11 @@
+﻿$FilePath = "D:\VM's\Machines\DevOps"
+# Gerar dados aleatórios e sobrescrever o arquivo
+$bytes = New-Object byte[] 2048
+$rand = New-Object Random
+$rand.NextBytes($bytes)
+
+# Sobrescrever o arquivo com dados aleatórios
+[IO.File]::WriteAllBytes($FilePath, $bytes)
+
+# Apagar o arquivo irrecuperavelmente
+Remove-Item -Path $FilePath -Force
